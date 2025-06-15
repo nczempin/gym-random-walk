f803736912fa128d7a9e860864df61d37159dd20
Nicolai Czempin <nicolai.czempin@gmail.com>
2025-06-15 22:39:38 +0200

    Bump version to 0.1.1 for security patch release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='gym_random_walk',
-      version='0.0.1',
+      version='0.1.1',
       install_requires=[
           'gym==0.7.4',
           'numpy==1.19.0',
